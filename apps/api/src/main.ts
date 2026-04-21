59dfc68453ae65581cda223310a21e08be52f242
Vladimir Agaev <agaev.vladimir@gmail.com>
2026-04-21 14:25:11 +0300

    chore: enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors();
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
 			transform: true,
 		}),
 	);
-	await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
